431d7247a29045169db912fcb59b0aedcc56d62f
xXWeIsLyXx <weihang.zheng@mail.utoronto.ca>
2022-12-29 15:24:56 -0800

    Initial Commit

---
+++ Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Course
+//
+//  Created by Weihang Zheng on 2022-12-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
